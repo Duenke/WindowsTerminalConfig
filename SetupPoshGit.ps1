de749244d771ff62f6c819a6f757b8c020c16f70
Jacob Duenke <jaduenke@microsoft.com>
2020-03-12 16:38:34 -0700

    Add conditions to Profile.ps1

--- SetupPoshGit.ps1
+++ SetupPoshGit.ps1
@@ -1,9 +1,34 @@
 # $PROFILE | Format-List -Force
 
+if (Get-Module -ListAvailable -Name posh-git) {
+  Write-Host "posh-git already installed..."
+} 
+else {
+    Write-Host "installing module: posh-git..."
+    Install-Module posh-git -Scope CurrentUser
+}
+if (Get-Module -ListAvailable -Name oh-my-posh) {
+    Write-Host "oh-my-posh already installed..."
+} 
+else {
+    Write-Host "installing module: oh-my-posh..."
+    Install-Module oh-my-posh -Scope CurrentUser
+}
+if (Get-Module -ListAvailable -Name PSReadLine) {
+    Write-Host "PSReadLine already installed..."
+} 
+else {
+    Write-Host "installing module: PSReadLine..."
+    Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
 Write-Host "removing excess profiles..."
 
 if ((Test-Path -Path $PROFILE.AllUsersAllHosts)) {
   Remove-Item -Path $PROFILE.AllUsersAllHosts
+}
+if ((Test-Path -Path $PROFILE.AllUsersCurrentHost)) {
+  Remove-Item -Path $PROFILE.AllUsersCurrentHost
 }
 if ((Test-Path -Path $PROFILE.CurrentUserAllHosts)) {
   Remove-Item -Path $PROFILE.CurrentUserAllHosts
@@ -11,36 +36,20 @@ if ((Test-Path -Path $PROFILE.CurrentUserAllHosts)) {
 if ((Test-Path -Path $PROFILE.CurrentUserCurrentHost)) {
   Remove-Item -Path $PROFILE.CurrentUserCurrentHost
 }
-if ((Test-Path -Path $PROFILE.AllUsersCurrentHost)) {
-  Remove-Item -Path $PROFILE.AllUsersCurrentHost
-}
 
 Write-Host "creating new profile..."
 
-New-Item -ItemType File -Path $PROFILE.AllUsersCurrentHost -Force
-Set-Content -Path $PROFILE.AllUsersCurrentHost -Value "
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox"
-
-if (Get-Module -ListAvailable -Name posh-git) {
-  Write-Host "posh-git already installed..."
-} 
-else {
-  Write-Host "installing module: posh-git..."
-  Install-Module posh-git -Scope CurrentUser
+New-Item -ItemType File -Path $PROFILE.AllUsersAllHosts -Force
+Set-Content -Path $PROFILE.AllUsersAllHosts -Value "
+if (`$Host.Name -eq 'ConsoleHost')
+{
+    Import-Module posh-git
+    Import-Module oh-my-posh
+    Set-Theme Paradox
 }
-if (Get-Module -ListAvailable -Name oh-my-posh) {
-  Write-Host "oh-my-posh already installed..."
-} 
-else {
-  Write-Host "installing module: oh-my-posh..."
-  Install-Module oh-my-posh -Scope CurrentUser
+elseif (`$Host.Name -like '*ISE Host')
+{
+    # Start-Steroids
+    # Import-Module PsIseProjectExplorer
 }
-if (Get-Module -ListAvailable -Name PSReadLine) {
-  Write-Host "PSReadLine already installed..."
-} 
-else {
-  Write-Host "installing module: PSReadLine..."
-  Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-}
+"
