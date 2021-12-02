3d284c4cfb0859ec1be006f581ec9f2c7e1e7ed3
Jacob Duenke <jaduenke@microsoft.com>
2021-12-02 09:26:37 -0800

    Add PIM scripts and .gitignore.

--- Setup-Powershell.ps1
+++ Setup-Powershell.ps1
@@ -50,75 +50,187 @@ catch {
 }
 
 function Dinks-AzContext {
-    `$AzContext = Get-AzContext;
+	[CmdletBinding()]
+	param (
+			
+	)
+	`$AzContext = Get-AzContext;
 
-    if ( -not `$AzContext )
-    {
-        Connect-AzAccount;
+	if ( -not `$AzContext )
+	{
+		Connect-AzAccount;
 
-        `$AzContext = Get-AzContext;
-    }
+		`$AzContext = Get-AzContext;
+	}
 
-    `$allContexts = (Get-AzContext -List | Sort-Object Account);
-    `$contextArray = @();
-    `$contextCount = 0;
-    `$contextInput = 0;
+	`$allContexts = (Get-AzContext -List | Sort-Object Account);
+	`$contextArray = @();
+	`$contextCount = 0;
+	`$contextInput = 0;
 
-    Foreach(`$context in `$allContexts){
-        if(`$context.Name.StartsWith('MSFT')){
-            `$contextArray += `$context;
-            Write-Host -ForegroundColor Gray ('[' + `$contextCount + '] - ' + `$context.Name);
-            `$contextCount++;
-        }
-    }
+	Foreach (`$context in `$allContexts) {
+		if (`$context.Name.StartsWith('MSFT')) {
+			`$contextArray += `$context;
+			Write-Host -ForegroundColor Gray ('[' + `$contextCount + '] - ' + `$context.Name);
+			`$contextCount++;
+		}
+	}
 
-    Write-Host;
-    `$contextInput = Read-Host 'Select Context'
+	Write-Host;
+	`$contextInput = Read-Host 'Select Context'
     
-    Write-Host;
-    Set-AzContext  -Context `$contextArray[`$contextInput];
+	Write-Host;
+	Set-AzContext  -Context `$contextArray[`$contextInput];
 }
 
 function Dinks-GitPrune {
-		try {
-			git checkout -q 'develop' | Out-Null;
-		}
-		catch {
-			Write-Host -ForegroundColor Red 'This directory is not a git repository!';
-			return;
-		}
+	[CmdletBinding()]
+	param (
+			
+	)
+	try {
+		git checkout -q 'develop' | Out-Null;
+	}
+	catch {
+		Write-Host -ForegroundColor Red 'This directory is not a git repository!';
+		return;
+	}
     
-    `$except = 'master', 'main', 'develop', 'a_dud';
-    `$availabeBranches = git branch | ForEach-Object { `$_.Substring(2, `$_.Length - 2) } | Where-Object { `$except -notcontains `$_ };
+	`$except = 'master', 'main', 'develop', 'a_dud';
+	`$availabeBranches = git branch | ForEach-Object { `$_.Substring(2, `$_.Length - 2) } | Where-Object { `$except -notcontains `$_ };
 
-    Write-Host -ForegroundColor Magenta 'Available branches on local machine: ';
-    `$index = -1;
-    foreach(`$branch in `$availabeBranches) {
-				`$index += 1;
-        Write-Host -ForegroundColor Yellow [`$index] `$branch;
-    }
+	Write-Host -ForegroundColor Magenta 'Available branches on local machine: ';
+	`$index = -1;
+	foreach (`$branch in `$availabeBranches) {
+		`$index += 1;
+		Write-Host -ForegroundColor Yellow [`$index] `$branch;
+	}
 
-    Write-Host;
-    Do {
-        `$saveIndex = Read-Host -Prompt 'Enter branch # to save';
+	Write-Host;
+	Do {
+		`$saveIndex = Read-Host -Prompt 'Enter branch # to save';
 
-				if(`$saveIndex -eq '') {
-					break;
-				}
-				else {
-					`$saveBranch = `$availabeBranches[`$saveIndex];
-					`$except += `$saveBranch;
-				}
-    } While(`$true)
+		if (`$saveIndex -eq '') {
+			break;
+		}
+		else {
+			`$saveBranch = `$availabeBranches[`$saveIndex];
+			`$except += `$saveBranch;
+		}
+	} While (`$true)
 
-    `$branchesToDelete = git branch | ForEach-Object { `$_.Substring(2, `$_.Length - 2) } | Where-Object { `$except -notcontains `$_ };
+	`$branchesToDelete = git branch | ForEach-Object { `$_.Substring(2, `$_.Length - 2) } | Where-Object { `$except -notcontains `$_ };
 
-    foreach(`$branch in `$branchesToDelete) {
-        Write-Host -ForegroundColor Yellow 'Deleting branch: ' `$branch;
-        git branch -d -f `$branch;
-    }
+	foreach (`$branch in `$branchesToDelete) {
+		Write-Host -ForegroundColor Yellow 'Deleting branch: ' `$branch;
+		git branch -d -f `$branch;
+	}
 
-    Write-Host -ForegroundColor Green 'Pulling branch: develop';
-    git pull;
+	Write-Host -ForegroundColor Green 'Pulling branch: develop';
+	git pull;
+}
+
+function Dinks-ActivatePimRoles {
+	[CmdletBinding()]
+	param (
+		[string]`$SubscriptionName,
+
+		[string]`$SubscriptionId,
+
+		[string]`$ResourceGroupName,
+
+		[string]`$ResourcePIMID,
+
+		[string]`$RoleName,
+
+		[string]`$RoleDefinitionPIMID,
+
+		[parameter(Mandatory = `$true)]
+		[string]`$Justification,
+
+		[parameter(Mandatory = `$true)]
+		[int]`$DurationInHours
+	)
+
+	if ([string]::IsNullOrEmpty(`$ResourcePIMID)) {
+		if (`$ResourceGroupName) {
+			`$ResourcePIMID = (Get-AzureADMSPrivilegedResource -ProviderId `"AzureResources`" -Filter `"ExternalId eq '/subscriptions/`$subscriptionID/resourceGroups/`$ResourceGroupName'`").Id
+		}
+		else {
+			`$ResourcePIMID = (Get-AzureADMSPrivilegedResource -ProviderId `"AzureResources`" -Filter `"ExternalId eq '/subscriptions/`$subscriptionID'`").Id
+		}
+	}
+
+	Write-Host `"Resource PIM id        : `$ResourcePIMID`"
+
+	if ([string]::IsNullOrEmpty(`$RoleDefinitionPIMID)) {
+		`$RoleDefinitionId = Get-AzRoleDefinition -Name `$RoleName | Select-Object -ExpandProperty Id
+		Write-Host `"Role definition id     : `$RoleDefinitionId`"
+
+		`$RoleDefinitionPIMID = Get-AzureADMSPrivilegedRoleDefinition -ProviderId `"AzureResources`" -Filter `"ExternalId eq '/subscriptions/`$subscriptionID/providers/Microsoft.Authorization/roleDefinitions/`$roleDefinitionID'`" -ResourceId `$ResourcePIMID  | Select-Object -ExpandProperty Id
+	}
+
+	Write-Host `"Role definition PIM id : `$RoleDefinitionPIMID`"
+
+	`$schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
+	`$schedule.Type = `"Once`"
+	`$schedule.StartDateTime = (Get-Date).ToUniversalTime()
+	`$schedule.endDateTime = `$schedule.StartDateTime.AddHours(`$DurationInHours)
+
+	Write-Debug (`$schedule)
+
+	`$AzContext = Get-AzContext
+	`$ADUserObj = Get-AzureADUser -ObjectId `$AzContext.Account.Id
+
+	`$params = @{
+		AssignmentState  = `"Active`";
+		ProviderId       = `"AzureResources`";
+		Reason           = `$Justification; 
+		ResourceId       = `$ResourcePIMID;
+		RoleDefinitionId = `$RoleDefinitionPIMID;
+		Type             = `"UserAdd`";
+		Schedule         = `$schedule;
+		SubjectId        = `$ADUserObj.ObjectId
+	}
+
+	Open-AzureADMSPrivilegedRoleAssignmentRequest @params
+}
+
+function Dinks-ListPimRoles {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	try {
+		`$AzContext = Get-AzContext;
+	
+		if (-not `$AzContext) {
+			Connect-AzAccount;
+			`$AzContext = Get-AzContext;
+		}
+	
+		Connect-AzureAD -TenantId `$AzContext.Tenant.TenantId -AccountId `$AzContext.Account.Id;
+		# Get-AzureADUser;
+
+		Write-Host -ForegroundColor Cyan `"Dinks-ActivatePimRoles -SubscriptionId 01151b44-dd2b-4585-a947-700f01bcc8d0 -RoleName Contributor -ResourceGroupName ReconPPE-SouthCentralUS-rg -Justification 'dev access' -DurationInHours 8 -ErrorAction Stop`"
+		Write-Host -ForegroundColor Cyan `"Dinks-ActivatePimRoles -SubscriptionId 01151b44-dd2b-4585-a947-700f01bcc8d0 -RoleName Contributor -ResourceGroupName ReconPPE-EastUS2-rg -Justification 'dev access' -DurationInHours 8 -ErrorAction Stop`"
+	}
+	catch {
+		`$TerminatingError = `$_;
+		`$ErrorTable = @{
+			LineNumber   = `$TerminatingError.InvocationInfo.ScriptLineNumber;
+			ScriptName   = `$TerminatingError.InvocationInfo.ScriptName;
+			LineContent  = `$TerminatingError.InvocationInfo.Line.Trim(`" `", `"`t`");
+			ErrorMessage = `$TerminatingError.Exception.Message;
+		}
+	
+		Write-Host;
+		Write-Host -ForegroundColor Red `"A terminating error occured!`";
+		Write-Host -ForegroundColor Red `"Details below:`";
+		`$ErrorTable | Format-Table -AutoSize;
+	
+		Exit 1;
+	}
 }
 "
